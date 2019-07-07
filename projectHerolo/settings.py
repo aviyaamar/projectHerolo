0de7306a1cd26b37a680cdcad9c8e9d4706a2149
aviyaamar <aviyaamar1@gmail.com>
2019-07-07 20:55:19 +0300

    seconf version

--- projectHerolo/settings.py
+++ projectHerolo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,6 +133,8 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 
 REST_FRAMEWORK = {
